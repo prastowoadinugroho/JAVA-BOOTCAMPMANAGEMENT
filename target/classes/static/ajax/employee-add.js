b27f50f157fc72676c3c3f2f7784670536482f85
PrastowoAdi <prastowoadinugroho@gmail.com>
2021-07-01 16:28:45 +0700

    Delete File
    Delete File

--- target/classes/static/ajax/employee-add.js
+++
@@ -1,6 +0,0 @@
-$(document).ready( function () {
-    $('#classes').select2().val(null).trigger('change');
-    $('#job').select2().val(null).trigger('change');
-    $('#jobStatus').select2().val(null).trigger('change');
-});
-
